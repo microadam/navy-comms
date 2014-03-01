2797ba415007607125576ef9a0462221ac28affe
Adam Duncan <adam.duncan@clock.co.uk>
2014-03-01 18:00:56 +0000

    Added bootstrap test

---
+++ test/bootstrap.test.js
@@ -0,0 +1,16 @@
+var should = require('should')
+  , bootstrap = require('../bootstrap')
+
+describe('bootstrap', function () {
+
+  it('should load all required modules', function () {
+      bootstrap(function (serviceLocator) {
+        should.exist(serviceLocator.db)
+        should.exist(serviceLocator.app)
+        should.exist(serviceLocator.config)
+        should.exist(serviceLocator.connectToAdmiral)
+        should.exist(serviceLocator.socketClient)
+      })
+    })
+
+})
